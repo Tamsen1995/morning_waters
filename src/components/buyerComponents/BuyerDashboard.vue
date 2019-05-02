b74f9296ed2c88c700f76c220980e242500af1ed
lalaleonor <stefani.leonor@gmail.com>
2019-05-02 16:30:08 -0700

    Figured out layout for orders. only worked on BuyerDashboard and orders.css. removed Orders from Settings.css

--- src/components/buyerComponents/BuyerDashboard.vue
+++ src/components/buyerComponents/BuyerDashboard.vue
@@ -4,53 +4,82 @@
   <div
     class="responsive-component"
     v-responsive="{
-    small: el => el.width < 430 }"
+    small: el => el.width < 860 }"
     id="orders-home"
   >
     <body class="orders">
       <div class="container" id="orders">
         <div class="row">
-          <div class="col-8">
-            <div class="card" id="orders-main">
-              <!-- Order Status -->
-              <h4>Order Status [TimeStamp]</h4>
-              <h5>In Corrospondance (link to inbox), Awaiting Seller Confirmation, Seller Confirmed [timestamp]</h5>
+          <div class="card" id="order-process-timeline">
+            Buyer Ordered
 
-              <h4>If Seller confirmed:</h4>
-              <h5>
-                <ul>
-                  <li>Billing date [timestamp]</li>
-                  <li>Amount Billed:</li>
-                  <li>Shipping status</li>
-                </ul>
-              </h5>
-              <br>
+            Seller Confirmed
+            Seller Shipping selected 
+            Seller Shipped
+              Out for delivery
+              Arriving today
+            Buyer received 
 
-              <!-- Cart -->
-              <div>Shopping Cart component</div>
-              <div>invoice download (pdf/csv)</div>
-            </div>
+            Buyer Shipping Selected
+            Buyer Shipped
+              Out for delivery
+              Arriving today
+            Seller Received
+
+            Order Complete
+          </div>
+          <div class="card" id="orders-main">
+            <!-- Order Status -->
+            <h4>Order Status:</h4>
+            <!-- Link to Inbox, most recent message -->
+            <h5>In Corrospondance</h5>
+              <ul>  
+                <li>[Seller Name] sent message on [timestamp]</li><!-- Timestamp of most recent message recieved -->
+              </ul>
+            <h5> Awaiting Seller Confirmation</h5>
+            <h2>[Company Name] should get back to you within 3 business days.</h2>
+
+            <h5> Seller Confirmed [timestamp]</h5>
+            <!-- If Seller Confirmed -->
+            <h5>
+              <ul>
+                <li>Billing date [timestamp]</li>
+                <li>Amount Billed:</li>
+              </ul>
+
+            </h5>
+            <h5> [link] view order </h5>
+            <br>
+          </div>
+          <br>
+
+          <!-- Shipping status-->
+          <!-- insert shipping timeline -->
+          <div class="card" id="shipping-main">
+            <h4>Shipping Status:</h4>
+            <h5> Seller has s[TimeStamp]</h5>
+            <h5> View Seller Shipping Selection</h5> <!-- Shipping invoice download -->
             <br>
 
-            <!-- Shipping -->
-            <div id="shipping-main">
-              <h4>Shipping Status</h4>
-              <h5>Awaiting Seller Confirmation, Seller Confirmed [timestamp]</h5>
-              <br>
+            <h5>Need to ship samples?</h5>
+              <div class="card">
+                Shipping UI
+              </div>
+          </div>
+          <br>
+        </div>
 
-              <h4>Shipping UI</h4>
-              <h5>Need to ship samples?</h5>
-            </div>
+        <div class="row">
+          <!-- Order History -->
+          <div class="card" id="order-history">
+            <h4>Order History</h4>
+
+            <h5> <span class="green">Order complete</span> <span class="red">Order incomplete</span></h5>
+            <h5>Payment Date [timestamp]</h5>
+            <h5>Shippment info [timestamp]</h5>
             <br>
-            <!-- Order History -->
-            <div id="order-history">
-              <h4>Order History</h4>
-              <h5>Payment Date [timestamp]</h5>
-              <h5>Shippment sent by [Company Name or Buyer Name] [timestamp]</h5>
-              <br>
-              <h4>Shipping info (link?)</h4>
-              <div>order history download (pdf/csv)</div>
-            </div>
+            <h4>Shipping info (link?)</h4>
+            <div>order history download (pdf/csv)</div>
           </div>
         </div>
       </div>
@@ -61,15 +90,19 @@
 
 <script>
 import BuyerHeader from "@/components/buyerComponents/BuyerHeader.vue";
+import { ResponsiveDirective } from 'vue-responsive-components';
 
 export default {
   components: {
     BuyerHeader
-  }
+  },
+    directives: {
+    responsive: ResponsiveDirective
+  },
 };
 </script>
 
 <style scoped>
 @import url("https://fonts.googleapis.com/css?family=Lato|Roboto");
-@import "../../assets/css/settings.css";
+@import "../../assets/css/orders.css";
 </style>
