afb068e453084bd2fb9509a617146cf37007be87
Tamsen1995 <Tamanhbui95@gmail.com>
2019-05-06 14:29:16 -0700

    [ADD] The buyer inbox is now segmented into order correspondacnes as well as quote request correspondances

--- src/components/buyerComponents/BuyerInbox.vue
+++ src/components/buyerComponents/BuyerInbox.vue
@@ -12,10 +12,10 @@
 
     <br>
     <br>
+    <!-- this is for the actually purchased orders -->
 
     <div id="orders-list">
       <li v-for="(order, index) in orders" @click="goToCorrespondance(index)" v-bind:key="index">
-        <br>
         <br>
 
         <div v-for="(item, index) in order" v-bind:key="index">
@@ -31,10 +31,26 @@
         </div>
 
         <br>
-        <br>
       </li>
     </div>
+    <!--  -->
 
+    <!-- This is for the quote requests made by the buyer -->
+    <div id="quote-orders-list">
+      <li
+        v-for="(quoteOrder, index) in quoteOrders"
+        @click="goToQuoteCorrespondance(index)"
+        v-bind:key="
+        index"
+      >
+        <div v-for="(item, index) in quoteOrder" v-bind:key="index">
+          <br>
+          {{item}}
+          <br>
+        </div>
+      </li>
+    </div>
+    <!--  -->
     <div></div>
   </body>
 </div>
