b38ff878e8db301cc68884ef581088b094e01845
Tamsen1995 <Tamanhbui95@gmail.com>
2019-05-06 19:57:19 -0700

    ]

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './App';
-import router from './router';
-import { sync } from 'vuex-router-sync';
-import store from '@/store/store';
-import VueResource from 'vue-resource';
-import VModal from 'vue-js-modal';
-import Api from '@/services/Api';
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import { sync } from 'vuex-router-sync'
+import store from '@/store/store'
+import VueResource from 'vue-resource'
+import VModal from 'vue-js-modal'
+import Api from '@/services/Api'
 
-import BootstrapVue from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-import AuthenticationService from '@/services/AuthenticationService';
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import AuthenticationService from '@/services/AuthenticationService'
 
 Api().defaults.headers.common[
   'Authorization'
@@ -22,7 +22,7 @@ AuthenticationService.checkAuth()
 console.log('Reloading app')
 Vue.use(BootstrapVue)
 Vue.use(VueResource)
-Vue.use(VModal)
+Vue.use(VModal) //
 
 sync(store, router)
 
