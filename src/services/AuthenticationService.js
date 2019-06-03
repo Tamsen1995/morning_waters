1cfbb97103a8e718960fecb3d9aa27b10f714222
Tamsen1995 <Tamanhbui95@gmail.com>
2019-06-03 12:50:09 -0700

    [UPD] I am updating the buyer register

--- src/services/AuthenticationService.js
+++ src/services/AuthenticationService.js
@@ -33,6 +33,7 @@ export default {
     return Api().post('buyerLogin', credentials)
   },
   buyerRegister (credentials) {
+    console.log(`\nJSON.stringify ${JSON.stringify(credentials)}\n`) // TESTING
     return Api().post('buyerRegister', credentials)
   }
 }
