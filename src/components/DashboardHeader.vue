b3f6dadc3c4d59595795d0d45c130de87e98922d
Tamsen1995 <Tamanhbui95@gmail.com>
2019-06-13 19:04:29 -0700

    []

--- src/components/DashboardHeader.vue
+++ src/components/DashboardHeader.vue
@@ -16,106 +16,101 @@
         >
           Menu
           <i class="fas fa-bars"></i>
-        </button> -->
+        </button>-->
         <!-- <div class="collapse navbar-collapse" id="navbarResponsive"> -->
-          <ul class="navbar-nav ml-auto">
-            <!-- /dashboard/creditsScreen/creditsPurchasing -->
+        <ul class="navbar-nav ml-auto">
+          <!-- /dashboard/creditsScreen/creditsPurchasing -->
 
-            <!-- Orders -->
-            <li class="nav-item">
-              <router-link
-                class="nav-link"
-                to="/dashboard/pending_orders"
-                v-if="this.user == null"
-                style="padding-top:0px;padding-bottom:0px;padding-left:15px;"
-              >
-                <i class="fas fa-seedling" id="dash_icon"></i>
-              </router-link>
-              <router-link
-                class="nav-link"
-                to="/dashboard/pending_orders"
-                v-if="this.user == null"
-                style="padding-top:0px;padding-bottom:0px; margin-bottom:0px;padding-right:15px;"
-              >
-                Orders
-                <span class="badge badge-success">{{ this.credits }}</span>
-              </router-link>
-            </li>
+          <!-- Orders -->
+          <li class="nav-item">
+            <router-link
+              class="nav-link"
+              to="/dashboard/pending_orders"
+              v-if="this.user == null"
+              style="padding-top:0px;padding-bottom:0px;padding-left:15px;"
+            >
+              <i class="fas fa-seedling" id="dash_icon"></i>
+            </router-link>
+            <router-link
+              class="nav-link"
+              to="/dashboard/pending_orders"
+              v-if="this.user == null"
+              style="padding-top:0px;padding-bottom:0px; margin-bottom:0px;padding-right:15px;"
+            >
+              Orders
+              <span class="badge badge-success">{{ this.credits }}</span>
+            </router-link>
+          </li>
 
-            <!-- Inbox -->
-            <li class="nav-item">
-              <router-link
-                class="nav-link"
-                to="/dashboard/quoteRequestsScreen"
-                v-if="this.user == null"
-                style="padding-top:0px;padding-bottom:0px;padding-left:15px;"
-              >
-                <i class="fas fa-envelope" alt="Inbox" id="dash_icon"></i>
-              </router-link>
-              <router-link
-                class="nav-link"
-                to="/dashboard/quoteRequestsScreen"
-                v-if="this.user == null"
-                style="padding-top:0px;padding-bottom:0px; margin-bottom:0px;"
-              >
+          <!-- Inbox -->
+          <li class="nav-item">
+            <router-link
+              class="nav-link"
+              to="/dashboard/quoteRequestsScreen"
+              v-if="this.user == null"
+              style="padding-top:0px;padding-bottom:0px;padding-left:7px;"
+            >
+              <i class="fas fa-envelope" alt="Inbox" id="dash_icon"></i>
+            </router-link>
+            <router-link
+              class="nav-link"
+              to="/dashboard/quoteRequestsScreen"
+              v-if="this.user == null"
+              style="padding-top:0px;padding-bottom:0px; margin-bottom:0px;"
+            >
               Inbox
-                <span class="badge badge-info">{{itemsInCart}}</span>
-              </router-link>
-            </li>
+              <span class="badge badge-info">{{itemsInCart}}</span>
+            </router-link>
+          </li>
 
-            <!-- Profile -->
-            <li class="nav-item">
-              <router-link
-                class="nav-link"
-                to="/dashboard"
-                v-if="this.user == null"
-                style="padding-top:0px;padding-bottom:0px;padding-left:10px;"
-              >
-                <i class="fas fa-home" id="dash_icon"></i>
-              </router-link>
-              <router-link
-                class="nav-link"
-                to="/dashboard"
-                v-if="this.user == null"
-                style="padding-top:0px;padding-bottom:0px; margin-bottom:0px;"
-              >Profile</router-link>
-            </li>
+          <!-- Profile -->
+          <li class="nav-item">
+            <router-link
+              class="nav-link"
+              to="/dashboard"
+              v-if="this.user == null"
+              style="padding-top:0px;padding-bottom:0px;padding-left:10px;"
+            >
+              <i class="fas fa-home" id="dash_icon"></i>
+            </router-link>
+            <router-link
+              class="nav-link"
+              to="/dashboard"
+              v-if="this.user == null"
+              style="padding-top:0px;padding-bottom:0px; margin-bottom:0px;"
+            >Profile</router-link>
+          </li>
 
-            <!-- Settings -->
-            <li class="nav-item">
-              <router-link
-                class="nav-link"
-                to="/dashboard/settings"
-                v-if="this.user == null"
-                style="padding-top:0px;padding-bottom:0px;padding-left:15px;"
-              >
-                <i class="fas fa-sliders-h" id="dash_icon"></i> 
-              </router-link>
-              <router-link
-                class="nav-link"
-                to="/dashboard/settings"
-                v-if="this.user == null"
-                style="padding-top:0px;padding-bottom:0px; margin-bottom:0px;"
-              >Settings</router-link>
-            </li>
+          <!-- Settings -->
+          <li class="nav-item">
+            <router-link
+              class="nav-link"
+              to="/dashboard/settings"
+              v-if="this.user == null"
+              style="padding-top:0px;padding-bottom:0px;padding-left:15px;"
+            >
+              <i class="fas fa-sliders-h" id="dash_icon"></i>
+            </router-link>
+            <router-link
+              class="nav-link"
+              to="/dashboard/settings"
+              v-if="this.user == null"
+              style="padding-top:0px;padding-bottom:0px; margin-bottom:0px;"
+            >Settings</router-link>
+          </li>
 
-            <!-- Logout -->
-            <li class="nav-item nav-center" @click="logout()">
-              <router-link
-                class="nav-link"
-                to="/dashboard/settings"
-                v-if="this.user == null"
-
-              >
-                <i class="fas fa-sign-out-alt"></i>
+          <!-- Logout -->
+          <li class="nav-item nav-center" @click="logout()">
+            <router-link class="nav-link" to="/dashboard/settings" v-if="this.user == null">
+              <i class="fas fa-sign-out-alt"></i>
+              Logout
+            </router-link>
+            <!-- <router-link class="nav-link nav-center" to="/" style="margin-top:0px;">
                  Logout
-              </router-link>
-              <!-- <router-link class="nav-link nav-center" to="/" style="margin-top:0px;">
-                 Logout
-              </router-link> -->
-            </li>
-          </ul>
-        </div>
+            </router-link>-->
+          </li>
+        </ul>
+      </div>
       <!-- </div> -->
     </nav>
   </div>
