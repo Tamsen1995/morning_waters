d2bab37f133c37a38adb7f0d883e692eb90da012
Tamsen1995 <Tamanhbui95@gmail.com>
2019-07-03 01:36:00 -0700

    [UPD] Some design changes on the buyer dashboard

--- src/components/buyerComponents/BuyerDashboard.vue
+++ src/components/buyerComponents/BuyerDashboard.vue
@@ -13,67 +13,60 @@
 
       <!--  -->
 
-      <div v-for="(pendingOrder, index) in this.pendingOrders" v-bind:key="index">
-        <!-- Post confirmation ORDER STATUS -->
-        <md-app>
-          <md-app-toolbar class="md-primary">
-            <span>Order ID : {{pendingOrder.orderId}}</span>
-          </md-app-toolbar>
+      <!-- Post confirmation ORDER STATUS -->
+      <md-app
+        class="page-container"
+        v-for="(pendingOrder, index) in this.pendingOrders"
+        v-bind:key="index"
+        style="background-color: white;"
+        :md-elevation="5"
+      >
+        <md-app-toolbar class="md-primary" style="background-color: rgb(157, 83, 252)">
+          <span class="md-title">Order ID : {{pendingOrder.orderId}}</span>
+        </md-app-toolbar>
 
-          <md-app-drawer md-permanent="clipped">
-            <md-list>
-              <md-list-item>
-                <md-button
-                  class="btn-timeline"
-                  id="btn-timeline"
-                  type="submit"
-                  @click="goToOrderStatus(index)"
-                >Timeline</md-button>
-                <span class="md-list-item-text"></span>
-              </md-list-item>
+        <md-app-drawer md-permanent="clipped" style="background-color: opacity:1.0">
+          <md-list>
+            <md-list-item>
+              <md-button id="btn-timeline" type="submit" @click="goToOrderStatus(index)">Timeline</md-button>
+            </md-list-item>
 
-              <md-list-item>
-                <md-icon>send</md-icon>
+            <md-list-item>
+              <md-button id="btn-shippo" type="submit" @click="goToOrderStatus(index)">Shipping</md-button>
+            </md-list-item>
 
-                <md-button
-                  class="btn-timeline"
-                  id="btn-timeline"
-                  type="submit"
-                  @click="goToOrderStatus(index)"
-                >Timeline</md-button>
+            <md-list-item>
+              <md-button
+                id="btn-shippo"
+                type="submit"
+                style="background-color: rgb(205, 188, 227);"
+                @click="downloadInvoice(pendingOrder)"
+              >Download Invoice</md-button>
+            </md-list-item>
+          </md-list>
+        </md-app-drawer>
 
-                <span class="md-list-item-text">Shipping</span>
-              </md-list-item>
-            </md-list>
-          </md-app-drawer>
+        <md-app-content class="md-title">
+          Order Status: PAID
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />Order Confirmed: [DATE]
+        </md-app-content>
+      </md-app>
+      <!-- If Order Confirmed -->
 
-          <md-app-content>Order Status:</md-app-content>
-        </md-app>
-        <!-- If Order Confirmed -->
+      <!-- IF Order UNCONFIRMED -->
+      <!-- <h2 class="status-unconfirmed" style="color:red;">Order Unconfirmed</h2>  -->
 
-        <!-- IF Order UNCONFIRMED -->
-        <!-- <h2 class="status-unconfirmed" style="color:red;">Order Unconfirmed</h2>  -->
+      <!-- View Order Details, Add expandable Div -->
 
-        <!-- View Order Details, Add expandable Div -->
+      <!-- IF UNCONFIRMED -->
+      <!-- <button class="btn-danger" id="btn-confirm-order">Confirm Order</button> -->
 
-        <br />
-
-        <!-- IF UNCONFIRMED -->
-        <!-- <button class="btn-danger" id="btn-confirm-order">Confirm Order</button> -->
-
-        <!-- IF CONFIRMED -->
-        <button
-          class="btn-timeline"
-          id="btn-timeline"
-          type="submit"
-          @click="goToOrderStatus(index)"
-        >View Timeline</button>
-
-        <!-- Handle shipping -->
-        <button id="btn-shippo" type="submit">Shipping</button>
-
-        <a class="invoice-link pull-right" @click="downloadInvoice(pendingOrder)">Download Invoice</a>
-      </div>
+      <!-- IF CONFIRMED -->
     </div>
   </body>
 </div>
@@ -87,6 +80,17 @@
 @import "../../assets/css/timeline.css";
 @import url("https://use.fontawesome.com/releases/v5.8.1/css/all.css");
 @import "../../assets/css/orders.css";
+.page-container {
+  min-height: 270px;
+  border: 1px solid rgb(121, 161, 39);
+  background: #ffffff 0%;
+}
+
+.md-drawer {
+  border-right: 1px solid rgb(121, 161, 39);
+  background: #ffffff 0%;
+  max-width: 30%;
+}
 </style>
 
 
