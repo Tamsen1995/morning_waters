2e68fc9007655c1e5025acd6697c10528106e971
Tamsen1995 <Tamanhbui95@gmail.com>
2019-07-03 02:08:30 -0700

    [UPD] Material design to the settings

--- src/components/buyerComponents/BuyerSettings.vue
+++ src/components/buyerComponents/BuyerSettings.vue
@@ -1,6 +1,6 @@
 <template>
 <div id="app">
-  <buyer-header/>
+  <buyer-header />
   <div class="responsive-component" v-responsive="{
       small: el => el.width < 430
       }">
@@ -8,7 +8,7 @@
       <div id="settings-page">
         <div id="settings-nav">
           <h3>Settings</h3>
-          <br>
+          <br />
           <tabs
             :tabs="tabs"
             :currentTab="currentTab"
@@ -21,86 +21,83 @@
         </div>
 
         <div class="settings-content">
-          <br>
+          <br />
+
+          <!--  -->
           <div v-show="currentTab === 'profile1'">
-            <br>
-            <!-- Edit Profile info -->
-            <div>
-              <h4>Edit Main Profile Info:</h4>
-              <p>(Services information can be added & edited directly on your profile.)</p>
-              <br>
-            </div>
-            <!-- input for Name -->
-            <div class="form-row">
-              <label horizontal for="name" class="col-sm col-form-label">Name:</label>
-              <div class="col-8">
-                <input v-model="name" class="form-control" type="text" placeholder="Edit your name">
-              </div>
-            </div>
+            <!--  -->
+            <!--  -->
+            <md-card md-with-hover>
+              <md-ripple>
+                <md-card-header>
+                  <div class="md-title">Edit Main Profile Info:</div>
+                  <div
+                    class="md-subhead"
+                  >(Services information can be added & edited directly on your profile.)</div>
+                </md-card-header>
 
-            <!-- input for email address -->
-            <div class="form-row">
-              <label horizontal for="email" class="col-sm col-form-label">Email:</label>
-              <div class="col-8">
-                <input
-                  v-model="email"
-                  class="form-control"
-                  type="email"
-                  placeholder="Edit your email"
-                >
-              </div>
-            </div>
+                <md-card-content>
+                  <br />
+                  <!-- Edit Profile info -->
+                  <div class="form-row">
+                    <md-field>
+                      <label>Name:</label>
+                      <md-input v-model="name"></md-input>
+                    </md-field>
+                  </div>
+                  <div class="form-row">
+                    <md-field>
+                      <label>Email:</label>
+                      <md-input v-model="email"></md-input>
+                    </md-field>
+                  </div>
 
-            <!-- input your phone number -->
-            <div class="form-row">
-              <label horizontal for="name" class="col-sm col-form-label">Phone Number:</label>
-              <div class="col-8">
-                <input
-                  v-model="number"
-                  class="form-control"
-                  type="number"
-                  placeholder="Edit your phone number"
-                >
-              </div>
-            </div>
+                  <div class="form-row">
+                    <md-field>
+                      <label>Phone Number:</label>
+                      <md-input v-model="number"></md-input>
+                    </md-field>
+                  </div>
 
-            <!-- Input for Billing Address -->
-            <!-- Add more formatting to this section: Maybe even map app -->
-            <div class="form-row">
-              <label horizontal for="name" class="col-sm col-form-label">Billing Address:</label>
-              <div class="col-8">
-                <input
-                  v-model="billingAddress"
-                  class="form-control"
-                  type="text"
-                  placeholder="Edit your billing address"
-                  description="Edit your billing address."
-                >
-              </div>
-            </div>
-            <!-- Input for About -->
-            <div class="form-group row">
-              <label for="About" class="col-sm col-form-label">About:</label>
-              <div class="col-sm-12">
-                <textarea
-                  v-model="about"
-                  class="form-control"
-                  placeholder="Tell us about your company"
-                ></textarea>
-              </div>
-            </div>
+                  <!-- Input for Billing Address -->
+                  <!-- Add more formatting to this section: Maybe even map app -->
+                  <div class="form-row">
+                    <md-field>
+                      <label>Billing Address:</label>
+                      <md-input v-model="billingAddress"></md-input>
+                    </md-field>
+                  </div>
 
-            <br>
+                  <!-- Input for About -->
+                  <div class="form-group row">
+                    <md-field>
+                      <label>About:</label>
+                      <md-textarea v-model="about" class="form-control"></md-textarea>
+                    </md-field>
+                  </div>
+                </md-card-content>
+
+                <md-card-actions>
+                  <md-button>Action</md-button>
+                  <md-button>Action</md-button>
+                </md-card-actions>
+              </md-ripple>
+            </md-card>
+            <!--  -->
+            <!--  -->
+
+            <br />
             <!-- <button class="btn btn-default btn-xl pull-right" @click="updateProfile">Update Profile</button> -->
           </div>
+          <!--  -->
 
           <div v-if="currentTab === 'billing3'">
             <billing-tab></billing-tab>
           </div>
           <div v-show="currentTab === 'password4'">
             <h4>Edit Password:</h4>
-            <br>
-            <br>
+            <br />
+            <br />
             <!-- Input old password -->
             <div class="form-row">
               <label horizontal for="oldPass" class="col-sm col-form-label">Old Password:</label>
@@ -110,7 +107,7 @@
                   class="form-control"
                   type="password"
                   placeholder="Enter your old password"
-                >
+                />
               </div>
             </div>
             <!-- input for new password -->
@@ -122,7 +119,7 @@
                   class="form-control"
                   type="password"
                   placeholder="Enter your new password"
-                >
+                />
               </div>
             </div>
             <!-- Confirm new password -->
@@ -138,11 +135,11 @@
                   class="form-control"
                   type="password"
                   placeholder="Re-enter your new password please"
-                >
+                />
               </div>
             </div>
 
-            <br>
+            <br />
             <button class="btn btn-outline btn-xl pull-right" @click="updatePassword">
               <p>Update Password</p>
             </button>
