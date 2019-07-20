c70dc5e2e6f02ca121e69119b145b52957d532c7
Tamsen1995 <Tamanhbui95@gmail.com>
2019-07-20 15:33:43 -0700

    [DEPLOY]

--- src/components/ComingSoon.vue
+++ src/components/ComingSoon.vue
@@ -1,14 +1,39 @@
 <template>
 <div>
+  <page-header></page-header>
   <body>
-    <h1>Coming Soon</h1>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+
+    <md-empty-state
+      style="background-color: grey; color: white;"
+      md-rounded
+      md-label="Coming Soon"
+      md-description="We'll contact you when our services are fully operational."
+    >
+      <img
+        src="../assets/css/TINY_LOGO_WHITE.png"
+        alt="FIG ANALYTICS"
+        class="center"
+        id="login-logo"
+      />
+    </md-empty-state>
   </body>
 </div>
 </template>
 
 <script>
+import PageHeader from "@/components/Header.vue";
+
 export default {};
 </script>
 
-<style>
+<style scoped>
+@import "../assets/css/forms.css";
+@import url("https://fonts.googleapis.com/css?family=Open+Sans|Roboto");
 </style>
