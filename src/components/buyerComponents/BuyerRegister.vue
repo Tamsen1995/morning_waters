7ed782f1e0083c0bd782c3ed1c8ff4490ab2714c
Tamsen1995 <Tamanhbui95@gmail.com>
2019-07-29 14:13:35 -0700

    []

--- src/components/buyerComponents/BuyerRegister.vue
+++ src/components/buyerComponents/BuyerRegister.vue
@@ -1,17 +1,14 @@
 <template>
 <div>
   <buyer-header></buyer-header>
-  <div
-    class="responsive-component"
-    v-responsive="{
+  <div class="responsive-component" v-responsive="{
       small: el => el.width < 860
-      }"
-  >
+      }">
     <body>
       <div class="fixed-horizontal-center">
         <!-- Loadng animation -->
         <div class="loader" v-if="loadingFlag== 2">Loading...</div>
-        
+
         <!-- Form -->
         <md-card id="register">
           <md-card-header>
@@ -19,52 +16,81 @@
           </md-card-header>
 
           <md-card-content>
-
             <div class="error" v-html="error" />
-              <form>
-                <md-steppers :md-active-step.sync="active" md-linear md-vertical >
-                  <md-step id="first" :md-done.sync="first" md-label="Personal Info" md-description="Required">
-
-                    <div class="form-group row">
-                      <div class="col-sm-12">
-                        <md-field>
-                          <label>Name :</label>
-                          <md-input class="form-control" v-model="name"></md-input>
-                        </md-field>
-                      </div>
+            <form>
+              <md-steppers :md-active-step.sync="active" md-linear md-vertical>
+                <md-step
+                  id="first"
+                  :md-done.sync="first"
+                  md-label="Personal Info"
+                  md-description="Required"
+                >
+                  <div class="form-group row">
+                    <div class="col-sm-12">
+                      <md-field>
+                        <label>Name :</label>
+                        <md-input
+                          class="form-control"
+                          v-model="name"
+                          v-on:keyup.enter="setDone('first', 'second')"
+                        ></md-input>
+                      </md-field>
                     </div>
+                  </div>
 
-                    <div class="form-group row">
-                      <div class="col-sm-12">
-                        <md-field>
-                          <label>Email :</label>
-                          <md-input class="form-control" v-model="email"></md-input>
-                        </md-field>
-                      </div>
+                  <div class="form-group row">
+                    <div class="col-sm-12">
+                      <md-field>
+                        <label>Email :</label>
+                        <md-input
+                          class="form-control"
+                          v-model="email"
+                          v-on:keyup.enter="setDone('first', 'second')"
+                        ></md-input>
+                      </md-field>
                     </div>
+                  </div>
 
-                    <div class="form-group row">
-                      <div class="col-sm-12">
-                        <md-field>
-                          <label>password :</label>
-                          <md-input type="password" class="form-control" v-model="password"></md-input>
-                        </md-field>
-                      </div>
+                  <div class="form-group row">
+                    <div class="col-sm-12">
+                      <md-field>
+                        <label>password :</label>
+                        <md-input
+                          type="password"
+                          class="form-control"
+                          v-model="password"
+                          v-on:keyup.enter="setDone('first', 'second')"
+                        ></md-input>
+                      </md-field>
                     </div>
+                  </div>
 
-                    <div class="form-group row">
-                      <div class="col-sm-12">
-                        <md-field>
-                          <label>Confirm Password :</label>
-                          <md-input type="password" class="form-control" v-model="passwordConfirm"></md-input>
-                        </md-field>
-                      </div>
+                  <div class="form-group row">
+                    <div class="col-sm-12">
+                      <md-field>
+                        <label>Confirm Password :</label>
+                        <md-input
+                          type="password"
+                          class="form-control"
+                          v-model="passwordConfirm"
+                          v-on:keyup.enter="setDone('first', 'second')"
+                        ></md-input>
+                      </md-field>
                     </div>
-                    <md-button style="border-radius:15px;" class="md-raised md-primary" @click="setDone('first', 'second')">Continue</md-button>
+                  </div>
+                  <md-button
+                    style="border-radius:15px;"
+                    class="md-raised md-primary"
+                    @click="setDone('first', 'second')"
+                  >Continue</md-button>
                 </md-step>
-          
-                <md-step id="second" :md-done.sync="second" md-label="Organization Info" md-description="Required">
 
+                <md-step
+                  id="second"
+                  :md-done.sync="second"
+                  md-label="Organization Info"
+                  md-description="Required"
+                >
                   <br />
                   <!-- Street -->
                   <div class="form-group row">
@@ -72,7 +98,11 @@
                       <!--  -->
                       <md-field>
                         <label>Street :</label>
-                        <md-input class="form-control" v-model="address.street"></md-input>
+                        <md-input
+                          class="form-control"
+                          v-model="address.street"
+                          v-on:keyup.enter="setDone('second', 'third')"
+                        ></md-input>
                       </md-field>
                       <!--  -->
                     </div>
@@ -84,7 +114,11 @@
                       <!--  -->
                       <md-field>
                         <label>City :</label>
-                        <md-input class="form-control" v-model="address.city"></md-input>
+                        <md-input
+                          class="form-control"
+                          v-model="address.city"
+                          v-on:keyup.enter="setDone('second', 'third')"
+                        ></md-input>
                       </md-field>
                       <!--  -->
                     </div>
@@ -96,7 +130,11 @@
                       <!--  -->
                       <md-field>
                         <label>State :</label>
-                        <md-input class="form-control" v-model="address.state"></md-input>
+                        <md-input
+                          class="form-control"
+                          v-model="address.state"
+                          v-on:keyup.enter="setDone('second', 'third')"
+                        ></md-input>
                       </md-field>
                       <!--  -->
                     </div>
@@ -105,7 +143,11 @@
                   <!-- Zip -->
                   <md-field>
                     <label>Zip :</label>
-                    <md-input class="form-control" v-model="address.zip"></md-input>
+                    <md-input
+                      class="form-control"
+                      v-model="address.zip"
+                      v-on:keyup.enter="setDone('second', 'third')"
+                    ></md-input>
                   </md-field>
 
                   <!-- Country -->
@@ -114,7 +156,11 @@
                       <!--  -->
                       <md-field>
                         <label>Country :</label>
-                        <md-input class="form-control" v-model="address.country"></md-input>
+                        <md-input
+                          class="form-control"
+                          v-model="address.country"
+                          v-on:keyup.enter="setDone('second', 'third')"
+                        ></md-input>
                       </md-field>
                       <!--  -->
                     </div>
@@ -127,15 +173,29 @@
                       <md-field>
                         <label>Phone Number:</label>
 
-                        <md-input v-model="number" type="text" placeholder="Enter your phone number"></md-input>
+                        <md-input
+                          v-model="number"
+                          type="text"
+                          placeholder="Enter your phone number"
+                          v-on:keyup.enter="setDone('second', 'third')"
+                        ></md-input>
                       </md-field>
                     </div>
                   </div>
-                  <md-button style="border-radius:15px;" class="md-raised md-primary" @click="setDone('second', 'third')">Continue</md-button>
+                  <md-button
+                    style="border-radius:15px;"
+                    class="md-raised md-primary"
+                    @click="setDone('second', 'third')"
+                  >Continue</md-button>
                 </md-step>
-              
+
                 <!-- Terms&Conditions -->
-                <md-step id="third" :md-done.sync="third" md-label="Terms & Conditions" md-description="Required">
+                <md-step
+                  id="third"
+                  :md-done.sync="third"
+                  md-label="Terms & Conditions"
+                  md-description="Required"
+                >
                   <div
                     name="termly-embed"
                     data-id="5c4ca188-e4a8-4d86-b760-1a88ff2915f8"
@@ -143,15 +203,28 @@
                   ></div>
                   <div class="form-check">
                     <input type="checkbox" class="form-check-input" id="termsCheck" />
-                    <label class="form-check-label" for="termsCheck" style="padding-left:20px;">Accept</label>
+                    <label
+                      class="form-check-label"
+                      for="termsCheck"
+                      style="padding-left:20px;"
+                    >Accept</label>
                   </div>
                   <br />
-                  
-                  <md-button style="border-radius:15px;" class="md-raised md-primary" @click="setDone('third', 'fourth')">Continue</md-button>
+
+                  <md-button
+                    style="border-radius:15px;"
+                    class="md-raised md-primary"
+                    @click="setDone('third', 'fourth')"
+                  >Continue</md-button>
                 </md-step>
-                
+
                 <!-- Privacy -->
-                <md-step id="fourth" :md-done.sync="fourth" md-label="Privacy" md-description="Required" >
+                <md-step
+                  id="fourth"
+                  :md-done.sync="fourth"
+                  md-label="Privacy"
+                  md-description="Required"
+                >
                   <div
                     name="termly-embed"
                     data-id="5c4ca188-e4a8-4d86-b760-1a88ff2915f8"
@@ -159,17 +232,19 @@
                   ></div>
                   <div class="form-check">
                     <input type="checkbox" class="form-check-input" id="privacyCheck" />
-                    <label class="form-check-label" for="privacyCheck" style="padding-left:20px;">Accept</label>
+                    <label
+                      class="form-check-label"
+                      for="privacyCheck"
+                      style="padding-left:20px;"
+                    >Accept</label>
                   </div>
                   <br />
-                  
+
                   <md-button class="md-raised md-primary" id="btn-login" @click="register">Sign Up</md-button>
                 </md-step>
               </md-steppers>
             </form>
-
           </md-card-content>
-
         </md-card>
         <br />
         <br />
