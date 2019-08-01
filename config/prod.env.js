c7beb473f3770c3e2eb712fb18d305f7d5f5768d
Tamsen1995 <Tamanhbui95@gmail.com>
2019-08-01 10:38:16 -0700

    [UPD] Adding testing keys to the prod and dev environment

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,7 +2,7 @@
 module.exports = {
   NODE_ENV: '"production"',
   stripe_public_key: '"pk_test_CLMSL40and9mdJdOgCRMbLfs"',
-  // SHIPPO_CLIENT_ID: '"shippo_test_cec19a3a037d7f159dc021f6b4db26407c0fcb38"',
+  SHIPPO_TEST_API_TOKEN: '"shippo_test_c78b517b07062943d690bbc5791e2ee0943ae929"',
   SHIPPO_CLIENT_ID: '"6c0b60adaeb84bf497bb66c8d522f806"', // the real one
   SHIPPO_CLIENT_SECRET: '"YpiEp5kiqN9J_NVt4Z4iZACX-yT2Zq0Y5rdrMCnTXek"',
   SHIPPO_OAUTH_LINK: "'https://goshippo.com/oauth/authorize?response_type=code&client_id=6c0b60adaeb84bf497bb66c8d522f806&scope=*&state='"
