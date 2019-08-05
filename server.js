ca9808f262a0699229c98cf2d61209c4ab252d9d
Tamsen1995 <Tamanhbui95@gmail.com>
2019-08-04 18:14:27 -0700

    [DEPLOY]

--- server.js
+++ server.js
@@ -2,15 +2,8 @@ const express = require('express')
 const path = require('path')
 const serveStatic = require('serve-static')
 
-var sslRedirect = require('heroku-ssl-redirect');
-
-
-
 let app = express()
 app.use(serveStatic(__dirname + '/dist'))
-
-app.use(sslRedirect());
-
 
 const port = process.env.PORT || 7000
 app.listen(port, () => {
