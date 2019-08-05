cdd07c9c2a93668bb7571eb20598b9ac52faf2d3
Tamsen1995 <Tamanhbui95@gmail.com>
2019-08-04 18:24:14 -0700

    [DEPLOY]

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const path = require('path')
 const serveStatic = require('serve-static')
+var enforce = require('express-sslify');
 
 let app = express()
 app.use(serveStatic(__dirname + '/dist'))
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 const port = process.env.PORT || 7000
 app.listen(port, () => {
