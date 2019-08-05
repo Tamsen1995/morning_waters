fe3029dca4d5f5642671981bf41148a3b5657c85
Tamsen1995 <Tamanhbui95@gmail.com>
2019-08-04 19:54:11 -0700

    [DEPLOY]

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const serveStatic = require('serve-static')
 
 let app = express()
 app.use(serveStatic(__dirname + '/dist'))
+if (window.location.protocol !== 'https:') window.location.href = 'https://yourdomain.com'
+
 
 const port = process.env.PORT || 7000
 app.listen(port, () => {
