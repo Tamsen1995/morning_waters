a8683906b1a57496723707c0c9d9af35c41ad9ca
Tamsen1995 <Tamanhbui95@gmail.com>
2019-08-06 19:46:13 -0700

    [UPD] transferring code to the seller side timeline. Next step is to gut it for re-use

--- src/components/sellerComponents/PendingOrderStatus.vue
+++ src/components/sellerComponents/PendingOrderStatus.vue
@@ -2,60 +2,64 @@
 <div class="responsive-component" v-responsive="{
   small: el => el.width < 860
   }">
-  <dashboard-header></dashboard-header>
+  <buyer-header></buyer-header>
+
   <body class="pending_orders">
     <div id="pending_orders_content">
       <div class="container current-order-status">
         <!-- Add tabs to toggle Current Orders/ Pending Orders-->
         <!-- Curent Orders -->
         <br />
+
         <div class="timeline-dashboard">
           <!-- <div v-for="(pendingOrder, index) in this.pendingOrders" v-bind:key="index"> -->
           <div>
             <div class="row">
               <div class="col-8">
+                <!-- TODO -->
+                <h3>[Seller Name]</h3>
+                <h2>[Seller Lab]</h2>
+
+                <!-- TODO -->
                 <h3>[Buyer Name]</h3>
                 <h2>[Buyer Lab]</h2>
                 <br />
                 <h3 style="color:green;">Order Confirmed [Date]</h3>
-                <h2 style="color:green;">Charged [$$$]</h2>
+                <h2 style="color:green;">Charged / Estimated revenue [$$$]</h2>
                 <br />
 
-                <!-- View Order Details, Add expandable Div -->
-                <div id="order_details">
-                  <h3>Order ID :</h3>
-                  <ol id="item_list">
-                    <h3>
-                      <li></li>
-                    </h3>
-                  </ol>
-                </div>
+                <!-- TODO : View Order Details, Add expandable Div -->
               </div>
             </div>
             <br />
 
+            <!-- TODO  -->
             <!-- Handle shipping -->
+
             <button
+              v-if="this.orderStatusInt >= 3"
               class="pull-right"
               id="btn-shippo"
               type="submit"
-              @click="confirmOrder(index)"
+              @click="redirectToShippo"
             >Handle Shipping</button>
             <br />
-
-            <a class="invoice-link">Download Invoice</a>
           </div>
 
           <hr />
         </div>
 
+        <!-- Step 0 : -->
         <div id="seller-timeline">
-          <!-- Step 1: Buyer Submits order -->
           <div class="row timeline-movement">
-            <div class="timeline-badge" id="status-complete">
-              <span style="font-size: 40px;">
-                <i class="fas fa-clipboard-list"></i>
-              </span>
+            <div class="timeline-badge" style="top: 25px;">
+              <md-card
+                class="md-primary timeline-badge"
+                md-theme="green-card"
+                style="background-color: green;"
+              >
+                <md-icon>backup</md-icon>
+              </md-card>
             </div>
 
             <!-- Order Submitted-->
@@ -64,16 +68,13 @@
                 <div class="col-sm-11">
                   <div class="timeline-panel buyer-side">
                     <ul class="timeline-panel-ul">
-                      <li>
-                        <span class="importo">[Buyer Name], [Lab Name]</span>
-                      </li>
-                      <li>
-                        <span class="importo">Submited Order</span>
-                      </li>
+                      <li>The order has been placed</li>
+
                       <li>
                         <p>
-                          <small class="text-muted">
-                            <i class="glyphicon glyphicon-time"></i> Timestamp
+                          <small class="text-muted" v-if="this.order">
+                            <i class="glyphicon glyphicon-time"></i>
+                            placed at : [{{this.order.order.createdAt}}]
                           </small>
                         </p>
                       </li>
@@ -83,13 +84,53 @@
               </div>
             </div>
           </div>
+          <!--  -->
+
+          <!-- Step 1: Buyer Submits order -->
+          <div class="row timeline-movement" v-if="this.orderStatusInt >= 1">
+            <div class="timeline-badge" style="top: 25px;">
+              <md-card
+                class="md-primary timeline-badge"
+                md-theme="green-card"
+                style="background-color: green;"
+              >
+                <md-icon>backup</md-icon>
+              </md-card>
+            </div>
+
+            <!-- Order Submitted-->
+            <div class="col-sm-6 timeline-item">
+              <div class="row">
+                <div class="col-sm-11">
+                  <div class="timeline-panel buyer-side">
+                    <ul class="timeline-panel-ul">
+                      <li>Seller submitted the negotiation for buyer review</li>
+
+                      <li>
+                        <p>
+                          <small class="text-muted">
+                            <i class="glyphicon glyphicon-time"></i> [Timestamp] [Example Date]
+                          </small>
+                        </p>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!--  -->
 
           <!-- Step 2: Seller Confirms Order -->
-          <div class="row timeline-movement">
-            <div class="timeline-badge" id="status-complete">
-              <span style="font-size: 40px;">
-                <i class="fas fa-clipboard-check"></i>
-              </span>
+          <div class="row timeline-movement" v-if="this.orderStatusInt >= 2">
+            <div class="timeline-badge" style="top: 25px;">
+              <md-card
+                class="md-primary timeline-badge"
+                md-theme="green-card"
+                style="background-color: green;"
+              >
+                <md-icon>backup</md-icon>
+              </md-card>
             </div>
 
             <div class="col-sm-6 timeline-item">
@@ -105,19 +146,12 @@
                   <div class="timeline-panel seller-side">
                     <!-- Pending Confirmation -->
                     <ul class="timeline-panel-ul">
-                      <li>
-                        <span class="importo">You Confirmed Order</span>
-                      </li>
-                      <li>
-                        <span class="importo">Buyer Charged [total billed]</span>
-                      </li>
-                      <li>
-                        <span class="causale">View Invoice</span>
-                      </li>
+                      <li>Buyer submitted the negotiation and the order has been submitted</li>
+
                       <li>
                         <p>
                           <small class="text-muted">
-                            <i class="glyphicon glyphicon-time"></i> [TimeStamp]
+                            <i class="glyphicon glyphicon-time"></i> [TimeStamp] [Example date]
                           </small>
                         </p>
                       </li>
@@ -126,131 +160,33 @@
                 </div>
               </div>
             </div>
+            <!--  -->
           </div>
 
-          <!-- Pre-Delivery Shipping Status-->
-          <div class="row timeline-movement">
-            <div class="timeline-badge" style="padding:5px;" id="status-incomplete">
-              <span style="font-size: 37px; padding-top: 20px;">
-                <i class="fas fa-dolly"></i>
-              </span>
+          <!-- Step 3-->
+          <div class="row timeline-movement" v-if="this.orderStatusInt >= 3">
+            <div class="timeline-badge" style="top: 25px;">
+              <md-card
+                class="md-primary timeline-badge"
+                md-theme="green-card"
+                style="background-color: green;"
+              >
+                <md-icon>local_shipping</md-icon>
+              </md-card>
             </div>
 
-            <div class="col-sm-6 timeline-item">
-              <div class="row">
-                <div class="col-sm-11"></div>
-              </div>
-            </div>
-
-            <div class="col-sm-6 timeline-item">
-              <div class="row">
-                <div class="col-sm-offset-1 col-sm-11">
-                  <div class="timeline-panel seller-side">
-                    <!-- Before Shippo status updates -->
-                    <ul class="timeline-panel-ul">
-                      <li>
-                        <span class="importo">Need to ship?</span>
-                      </li>
-                      <li>
-                        <span class="causale">Shipping UI Link.</span>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <!-- Post-Delivery Status (Replace above block) -->
-          <div class="row timeline-movement">
-            <div class="timeline-badge" id="status-incomplete" style="top: 25%;">
-              <span style="font-size: 40px;">
-                <i class="far fa-check-circle"></i>
-              </span>
-            </div>
-
-            <div class="col-sm-offset-6 col-sm-6 timeline-item">
-              <div class="row">
-                <div class="col-sm-offset-1 col-sm-11">
-                  <div class="timeline-panel seller-side">
-                    <ul class="timeline-panel-ul">
-                      <li>
-                        <span class="importo">[Delivered]</span>
-                      </li>
-                      <li>
-                        <span class="causale">Tracking Number</span>
-                      </li>
-                      <li>
-                        <p>
-                          <small class="text-muted">
-                            <i class="glyphicon glyphicon-time"></i> 11/09/2014
-                          </small>
-                        </p>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <!-- Buyer Pre-Delivery Shipping Status-->
-          <div class="row timeline-movement">
-            <div class="timeline-badge" style="padding:5px;" id="status-incomplete">
-              <span style="font-size: 37px; padding-top: 20px;">
-                <i class="fas fa-dolly"></i>
-              </span>
-            </div>
-
-            <div class="col-sm-6 timeline-item">
-              <div class="row">
-                <div class="col-sm-11">
-                  <div class="timeline-panel buyer-side">
-                    <!-- With Shippo Status updates -->
-                    <ul class="timeline-panel-ul">
-                      <li>
-                        <span class="importo">[Shipping status]</span>
-                      </li>
-                      <li>
-                        <span class="causale">Tracking Number</span>
-                      </li>
-                      <li>
-                        <p>
-                          <small class="text-muted">
-                            <i class="glyphicon glyphicon-time"></i> 11/09/2014
-                          </small>
-                        </p>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <!-- Buyer Post-Delivery Status (Replace above block) -->
-          <div class="row timeline-movement">
-            <div class="timeline-badge" id="status-incomplete" style="top: 25%;">
-              <span style="font-size: 40px;">
-                <i class="far fa-check-circle"></i>
-              </span>
-            </div>
-
+            <!-- Seller Side text card-->
             <div class="col-sm-6 timeline-item">
               <div class="row">
                 <div class="col-sm-11">
                   <div class="timeline-panel buyer-side">
                     <ul class="timeline-panel-ul">
-                      <li>
-                        <span class="importo">[Delivered]</span>
-                      </li>
-                      <li>
-                        <span class="causale">Tracking Number</span>
-                      </li>
+                      <li>Seller submitted Shipping logistics</li>
                       <li>
                         <p>
                           <small class="text-muted">
-                            <i class="glyphicon glyphicon-time"></i> 11/09/2014
+                            <i class="glyphicon glyphicon-time"></i>
+                            Date of confirmation : [ {{ shipping_confirmed_date }} ]
                           </small>
                         </p>
                       </li>
@@ -259,14 +195,62 @@
                 </div>
               </div>
             </div>
+            <!--  -->
+            <div
+              v-if="shippoOrder.shipping_cost && shippoOrder.shipping_cost_currency && shippoOrder.shipping_method"
+            >
+              Shipping Cost: {{shippoOrder.shipping_cost}} {{shippoOrder.shipping_cost_currency}}
+              Shipping Method: {{shippoOrder.shipping_method}}
+            </div>
+
+            <!-- shipping_method": null, "shipping_cost": null, "shipping_cost_currency": null -->
           </div>
-          <br />
-          <br />
-          <div class="row timeline-movement timeline-movement-top">
-            <div class="timeline-badge timeline-future-movement" style="top: 25px;">
-              <a href="#">
-                <span class="glyphicon glyphicon-plus"></span>
-              </a>
+          <!-- Here we can insert icons to indicate the shipping status -->
+
+          <div v-if="this.orderStatusInt >= 4">
+            <!-- Step 4: Seller Confirms Order -->
+            <div
+              class="row timeline-movement timeline-movement-top"
+              v-for="(transaction, index) in this.shippoOrder.transactions"
+              v-bind:key="index"
+            >
+              <div class="timeline-badge" style="top: 25px;">
+                <md-card
+                  class="md-primary timeline-badge"
+                  md-theme="green-card"
+                  style="background-color: green;"
+                >
+                  <md-icon>local_shipping</md-icon>
+                </md-card>
+              </div>
+
+              <div class="col-sm-6 timeline-item">
+                <div class="row">
+                  <div class="col-sm-11"></div>
+                </div>
+              </div>
+
+              <!-- Order Confirmed-->
+              <div class="col-sm-6 timeline-item">
+                <div class="row">
+                  <div class="col-sm-offset-1 col-sm-11">
+                    <div class="timeline-panel seller-side">
+                      <!-- Pending Confirmation -->
+                      <ul class="timeline-panel-ul">
+                        <li>Transaction number : [{{transaction.tracking_number}}]</li>
+                        <li>
+                          <p>
+                            <small class="text-muted">
+                              <i class="glyphicon glyphicon-time"></i> [Concept Prototype]
+                            </small>
+                          </p>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!--  -->
             </div>
           </div>
         </div>
