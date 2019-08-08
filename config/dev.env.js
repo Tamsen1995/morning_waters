97889e964d996ecd7322296d7b93d66f8e037499
Tamsen1995 <Tamanhbui95@gmail.com>
2019-08-08 10:51:37 -0700

    [UPD] Oauth flow works all the way up to redirect in the backend. now get the stripe_client_id

--- config/dev.env.js
+++ config/dev.env.js
@@ -9,5 +9,5 @@ module.exports = merge(prodEnv, {
   SHIPPO_CLIENT_ID: '"6c0b60adaeb84bf497bb66c8d522f806"', // the real one
   SHIPPO_CLIENT_SECRET: '"YpiEp5kiqN9J_NVt4Z4iZACX-yT2Zq0Y5rdrMCnTXek"',
   SHIPPO_OAUTH_LINK: "'https://goshippo.com/oauth/authorize?response_type=code&client_id=6c0b60adaeb84bf497bb66c8d522f806&scope=*&state='",
-  STRIPE_CONNECT_OAUTH_LINK: "'https://connect.stripe.com/express/oauth/authorize?redirect_uri=https://stripe.com/connect/default/oauth/test&client_id=ca_Fa3eRugg6XNzKWW7TYf3OXyY9p59fn0n&state='"
+  STRIPE_CONNECT_OAUTH_LINK: "'https://connect.stripe.com/express/oauth/authorize?redirect_uri=https://secure-river-14395.herokuapp.com/auth/stripe_connect/redirect&client_id=ca_Fa3eRugg6XNzKWW7TYf3OXyY9p59fn0n&state='"
 })
