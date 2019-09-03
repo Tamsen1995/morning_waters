2e63f1fe33f9c6fb7cd270f5158988ed28856e55
Tamsen1995 <Tamanhbui95@gmail.com>
2019-09-03 15:42:30 -0700

    [UPD] Offloading the buyer settings script

--- src/components/buyerComponents/BuyerSettings.vue
+++ src/components/buyerComponents/BuyerSettings.vue
@@ -35,6 +35,18 @@
                   </div>
                   <div class="form-row">
                     <md-field>
+                      <label>First Name:</label>
+                      <md-input v-model="firstName"></md-input>
+                    </md-field>
+                  </div>
+                  <div class="form-row">
+                    <md-field>
+                      <label>Last Name:</label>
+                      <md-input v-model="lastName"></md-input>
+                    </md-field>
+                  </div>
+                  <div class="form-row">
+                    <md-field>
                       <label>Email:</label>
                       <md-input v-model="email"></md-input>
                     </md-field>
@@ -138,88 +150,7 @@
 </div>
 </template>
 
-<script>
-import BuyerHeader from "@/components/buyerComponents/BuyerHeader";
-import BuyerServices from "@/services/BuyerServices";
-import BillingTab from "@/components/buyerComponents/BuyerSettingsBillingsTab.vue";
-import Tabs from "vue-tabs-with-active-line";
-import { ResponsiveDirective } from "vue-responsive-components";
-
-export default {
-  data() {
-    return {
-      name: "",
-      email: "",
-      number: "",
-      tabs: [
-        { title: "Profile", value: "profile1" },
-        { title: "Billing", value: "billing3" },
-        { title: "Password", value: "password4" }
-      ],
-      currentTab: "profile1",
-      user: null,
-      oldPass: "",
-      newPass: "",
-      confirmNewPass: ""
-    };
-  },
-  mounted() {
-    this.retrieveProfileInfo();
-  },
-  directives: {
-    responsive: ResponsiveDirective
-  },
-  components: {
-    Tabs,
-    BuyerHeader,
-    BillingTab
-  },
-  methods: {
-    async retrieveProfileInfo() {
-      try {
-        const buyerExtracted = this.$store.getters.getBuyerInfo;
-        const response = await BuyerServices.getBuyerProfileInfo(
-          buyerExtracted.id
-        );
-        const buyerInfo = response.data.buyer;
-        this.name = buyerInfo.name;
-        this.email = buyerInfo.email;
-      } catch (error) {
-        if (error) throw error;
-      }
-    },
-    async handleClick(newTab) {
-      try {
-        console.log(`\nThe new tab is : ${newTab}\n`); // TESTING
-        this.currentTab = newTab;
-      } catch (error) {
-        if (error) throw error;
-      }
-    },
-    async updatePassword() {
-      try {
-        const buyerExtracted = this.$store.getters.getBuyerInfo;
-
-        const updatePassInfo = {
-          email: buyerExtracted.email,
-          oldPass: this.oldPass,
-          newPass: this.newPass,
-          confirmNewPass: this.confirmNewPass
-        };
-
-        const response = await BuyerServices.updateBuyerPassword(
-          updatePassInfo
-        );
-
-        console.log(`\nBaby baby yes ! ${JSON.stringify(response)}\n`); // TESTING
-
-        // this.confirmNewPass = "TESTING";
-      } catch (error) {
-        if (error) throw error;
-      }
-    }
-  }
-};
+<script src="./scripts/buyerSettings.js">
 </script>
 
 <style scoped>
