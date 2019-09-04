be064fe1d182c9e60674e1b1a8f81c13cd196ab0
Tamsen1995 <Tamanhbui95@gmail.com>
2019-09-03 17:28:33 -0700

    [UPD] Some minor UX changes

--- src/components/buyerComponents/BuyerSettings.vue
+++ src/components/buyerComponents/BuyerSettings.vue
@@ -15,49 +15,47 @@
           <md-tab id="tab-profile" md-label="Profile">
             <!--  -->
             <!--  -->
-            <md-card md-with-hover>
-              <md-ripple>
-                <md-card-header>
-                  <div class="md-title">Edit Main Profile Info:</div>
-                  <div
-                    class="md-subhead"
-                  >(Services information can be added & edited directly on your profile.)</div>
-                </md-card-header>
+            <md-card>
+              <md-card-header>
+                <div class="md-title">Edit Main Profile Info:</div>
+                <div
+                  class="md-subhead"
+                >(Services information can be added & edited directly on your profile.)</div>
+              </md-card-header>
 
-                <md-card-content>
-                  <br />
-                  <!-- Edit Profile info -->
-                  <!-- <div class="form-row">
+              <md-card-content>
+                <br />
+                <!-- Edit Profile info -->
+                <!-- <div class="form-row">
                     <md-field>
                       <label>Name:</label>
                       <md-input v-model="name"></md-input>
                     </md-field>
-                  </div>-->
-                  <div class="form-row">
-                    <md-field>
-                      <label>First Name:</label>
-                      <md-input v-model="firstName"></md-input>
-                    </md-field>
-                  </div>
-                  <div class="form-row">
-                    <md-field>
-                      <label>Last Name:</label>
-                      <md-input v-model="lastName"></md-input>
-                    </md-field>
-                  </div>
-                  <div class="form-row">
-                    <md-field>
-                      <label>Email:</label>
-                      <md-input v-model="email"></md-input>
-                    </md-field>
-                  </div>
-                </md-card-content>
+                </div>-->
+                <div class="form-row">
+                  <md-field>
+                    <label>First Name:</label>
+                    <md-input v-model="firstName"></md-input>
+                  </md-field>
+                </div>
+                <div class="form-row">
+                  <md-field>
+                    <label>Last Name:</label>
+                    <md-input v-model="lastName"></md-input>
+                  </md-field>
+                </div>
+                <div class="form-row">
+                  <md-field>
+                    <label>Email:</label>
+                    <md-input v-model="email"></md-input>
+                  </md-field>
+                </div>
+              </md-card-content>
 
-                <md-button
-                  class="md-raised md-primary"
-                  @click="updateProfileInfo()"
-                >Submit profile edits</md-button>
-              </md-ripple>
+              <md-button
+                class="md-raised md-primary"
+                @click="updateProfileInfo()"
+              >Submit profile edits</md-button>
             </md-card>
             <!--  -->
             <!--  -->
@@ -70,56 +68,50 @@
           </md-tab>
           <md-tab id="tab-password" md-label="Password">
             <md-card>
-              <md-ripple>
-                <md-card-header>
-                  <div class="md-title">Edit Password:</div>
-                </md-card-header>
+              <md-card-header>
+                <div class="md-title">Edit Password:</div>
+              </md-card-header>
 
-                <br />
-                <br />
-                <md-card-content>
-                  <!-- Input old password -->
-                  <div class="form-row">
-                    <!-- <label horizontal for="oldPass" class="col-sm col-form-label"></label>
+              <br />
+              <br />
+              <md-card-content>
+                <!-- Input old password -->
+                <div class="form-row">
+                  <!-- <label horizontal for="oldPass" class="col-sm col-form-label"></label>
                   <div class="col-8">
                     <input
                       v-model="oldPass"
                       class="form-control"
                     />
-                    </div>-->
-
-                    <md-field>
-                      <label>Old Password:</label>
-                      <md-input
-                        placeholder="Enter your old password"
-                        type="password"
-                        v-model="oldPass"
-                      ></md-input>
-                    </md-field>
-                  </div>
-                  <!-- input for new password -->
-                  <md-field>
-                    <label>New Password:</label>
-                    <md-input
-                      v-model="newPass"
-                      type="password"
-                      placeholder="Enter your new password"
-                    ></md-input>
-                  </md-field>
+                  </div>-->
 
                   <md-field>
-                    <label>Confirm new password:</label>
+                    <label>Old Password:</label>
                     <md-input
-                      v-model="confirmNewPass"
+                      placeholder="Enter your old password"
                       type="password"
-                      placeholder="Re-enter your new password please"
+                      v-model="oldPass"
                     ></md-input>
                   </md-field>
+                </div>
+                <!-- input for new password -->
+                <md-field>
+                  <label>New Password:</label>
+                  <md-input v-model="newPass" type="password" placeholder="Enter your new password"></md-input>
+                </md-field>
 
-                  <br />
-                  <md-button @click="updatePassword" class="md-raised md-primary">Update Password</md-button>
-                </md-card-content>
-              </md-ripple>
+                <md-field>
+                  <label>Confirm new password:</label>
+                  <md-input
+                    v-model="confirmNewPass"
+                    type="password"
+                    placeholder="Re-enter your new password please"
+                  ></md-input>
+                </md-field>
+
+                <br />
+                <md-button @click="updatePassword" class="md-raised md-primary">Update Password</md-button>
+              </md-card-content>
             </md-card>
           </md-tab>
         </md-tabs>
