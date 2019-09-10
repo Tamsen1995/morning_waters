be7572816bd6808afbb6724d1871838c8f8e4671
Tamsen1995 <Tamanhbui95@gmail.com>
2019-09-10 11:42:52 -0700

    [DEPLOY]

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const path = require('path')
 const serveStatic = require('serve-static')
 
 let app = express()
+app.use(history({
+  // OPTIONAL: Includes more verbose logging
+  verbose: true
+}))
 app.use(serveStatic(__dirname + '/dist'))
+
 
 const port = process.env.PORT || 7000
 app.listen(port, () => {
