252844f8176609958ebc860d853f02875d02f021
Tamsen1995 <Tamanhbui95@gmail.com>
2019-09-18 13:48:07 -0700

    [ADD] hello X

--- src/components/sellerComponents/sellerDashboard/Dashboard.vue
+++ src/components/sellerComponents/sellerDashboard/Dashboard.vue
@@ -370,6 +370,10 @@
             v-for="(subservice, index) in this.subServicesToBeAdded"
             v-bind:key="index"
           >
+            <md-button
+              class="pull-right"
+              style="border-width:1px; border-color: #2d133a; color: #2d133a; border-style: ridge;"
+            >X</md-button>
             <!-- Service Title -->
             <md-field>
               <label>Sub-Item:</label>
