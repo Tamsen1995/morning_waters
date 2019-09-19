b80c1cf4acbf28a760e2832937f70feac89310f6
Tamsen1995 <Tamanhbui95@gmail.com>
2019-09-18 18:39:13 -0700

    [UPD] the service tags use a different component now. commas are possible

--- src/components/sellerComponents/sellerDashboard/Dashboard.vue
+++ src/components/sellerComponents/sellerDashboard/Dashboard.vue
@@ -394,13 +394,15 @@
             </md-field>
 
             <!-- Sub Service Tags -->
+
+            Tags:
             <md-field>
-              <label>Tags</label>
-              <md-chips
-                md-placeholder="Add a tag and click enter"
+              <input-tag
+                style="width:100%;"
+                placeholder="Add a tag and press enter / comma"
                 v-model="subServicesToBeAdded[index].serviceTags"
-                style="border-bottom: 1px inset; color: #a558e4;"
-              ></md-chips>
+                :add-tag-on-keys="[13,188]"
+              ></input-tag>
             </md-field>
 
             <!-- Pricing Block -->
